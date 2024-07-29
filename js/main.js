d9af8133eb5e77085d6f7c65cc10620562794867
Sam MBP <snevarez89@gmail.com>
2024-07-28 17:12:27 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,116 @@
+(function ($) {
+  "use strict";
+
+  // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 80
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top = 1) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus');
+});
+
+//getting all required elements
+const gallery = document.querySelectorAll(".image"),
+  previewBox = document.querySelector(".preview-box"),
+  previewImg = previewBox.querySelector("img"),
+  closeIcon = previewBox.querySelector(".icon"),
+  currentImg = previewBox.querySelector(".current-img"),
+  totalImg = previewBox.querySelector(".total-img"),
+  shadow = document.querySelector(".shadow");
+window.onload = () => {
+  for (let i = 0; i < gallery.length; i++) {
+    totalImg.textContent = gallery.length; //passing total img length to totalImg variable
+    let newIndex = i; //passing i value to newIndex variable
+    let clickedImgIndex; //creating new variable
+
+    gallery[i].onclick = () => {
+      clickedImgIndex = i; //passing cliked image index to created variable (clickedImgIndex)
+      function preview() {
+        currentImg.textContent = newIndex + 1; //passing current img index to currentImg varible with adding +1
+        let imageURL = gallery[newIndex].querySelector("img").src; //getting user clicked img url
+        previewImg.src = imageURL; //passing user clicked img url in previewImg src
+      }
+
+      preview(); //calling above function
+
+      const prevBtn = document.querySelector(".prev");
+      const nextBtn = document.querySelector(".next");
+      if (newIndex == 0) {
+        //if index value is equal to 0 then hide prevBtn
+        prevBtn.style.display = "none";
+      }
+      if (newIndex >= gallery.length - 1) {
+        //if index value is greater and equal to gallery length by -1 then hide nextBtn
+        nextBtn.style.display = "none";
+      }
+      prevBtn.onclick = () => {
+        newIndex--; //decrement index
+        if (newIndex == 0) {
+          preview();
+          prevBtn.style.display = "none";
+        } else {
+          preview();
+          nextBtn.style.display = "block";
+        }
+      };
+      nextBtn.onclick = () => {
+        newIndex++; //increment index
+        if (newIndex >= gallery.length - 1) {
+          preview();
+          nextBtn.style.display = "none";
+        } else {
+          preview();
+          prevBtn.style.display = "block";
+        }
+      };
+      document.querySelector("body").style.overflow = "hidden";
+      previewBox.classList.add("show");
+      shadow.style.display = "block";
+      closeIcon.onclick = () => {
+        newIndex = clickedImgIndex; //assigning user first clicked img index to newIndex
+        prevBtn.style.display = "block";
+        nextBtn.style.display = "block";
+        previewBox.classList.remove("show");
+        shadow.style.display = "none";
+        document.querySelector("body").style.overflow = "scroll";
+      };
+    };
+  }
+};
